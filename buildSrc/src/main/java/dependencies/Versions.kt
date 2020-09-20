68a6e09cc1dae5787136f5f58208e92e06b4d47f
kimdohun <kimdohun0104@gmail.com>
2020-09-20 15:44:45 +0900

    1.0.0 출시를 위한 버전 변경

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -6,9 +6,9 @@ object Versions {
     const val minSdkVersion = 16
     const val targetSdkVersion = 30
 
-    private const val majorVersion = 0
+    private const val majorVersion = 1
     private const val minorVersion = 0
-    private const val patchVersion = 8
+    private const val patchVersion = 0
 
     const val versionCode = (100 * majorVersion) + (10 * minorVersion) + patchVersion
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
