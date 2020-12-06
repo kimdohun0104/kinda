26ebac3008bc94fa32d0dba75863ab7cc7d0fc72
kimdohun0104 <kimdohun0104@gmail.com>
2020-12-06 22:04:04 +0900

    SideEffectHandlerDslBuilder.kt 수정

--- kinda-dsl/src/main/java/dohun/kim/kinda/kinda_dsl/SideEffectHandlerDslBuilder.kt
+++ kinda-dsl/src/main/java/dohun/kim/kinda/kinda_dsl/SideEffectHandlerDslBuilder.kt
@@ -7,19 +7,19 @@ import dohun.kim.kinda.kinda_core.KindaState
 
 class SideEffectHandlerDslBuilder<S : KindaState, E : KindaEvent, SE : KindaSideEffect> {
 
-    val sideEffectHandleMap = HashMap<Class<SE>, suspend () -> E>()
+    val sideEffectHandleMap = HashMap<Class<SE>, suspend (SE) -> E>()
 
     inline fun <reified SIDE_EFFECT : SE> whenSideEffect(
-        noinline next: suspend () -> E
+        noinline next: suspend (SIDE_EFFECT) -> E
     ) {
-        sideEffectHandleMap[SIDE_EFFECT::class.java as Class<SE>] = next
+        sideEffectHandleMap[SIDE_EFFECT::class.java as Class<SE>] = next as suspend (SE) -> E
     }
 
     fun build() = object : KindaSideEffectHandler<S, E, SE> {
         override suspend fun handle(sideEffect: SE): E {
             sideEffectHandleMap.keys.forEach { key ->
                 if (key.isInstance(sideEffect)) {
-                    return sideEffectHandleMap[key]!!.invoke()
+                    return sideEffectHandleMap[key]!!.invoke(sideEffect)
                 }
             }
             throw IllegalStateException("SideEffect not handled ${sideEffect::class}")
