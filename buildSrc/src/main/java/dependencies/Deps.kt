c9041366e0f469db3bb3b16693fc3e2e4267e827
dohunkim <kimdohun0104@gmail.com>
2021-12-29 22:04:26 +0900

    kinda core multiplatform 모듈 생성

--- buildSrc/src/main/java/dependencies/Deps.kt
+++ buildSrc/src/main/java/dependencies/Deps.kt
@@ -13,8 +13,8 @@ object Deps {
     }
 
     object Kotlin {
-        const val version = "1.4.10"
-        const val coroutineVersion = "1.4.3"
+        const val version = "1.6.10"
+        const val coroutineVersion = "1.6.0-native-mt"
         const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$version"
         const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion"
         const val coroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion"
--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -2,9 +2,9 @@ package dependencies
 
 object Versions {
 
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 31
     const val minSdkVersion = 16
-    const val targetSdkVersion = 30
+    const val targetSdkVersion = 31
 
     private const val majorVersion = 1
     private const val minorVersion = 4
